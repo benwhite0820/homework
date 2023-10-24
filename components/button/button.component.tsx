7c8aec24d84f14cb5ff65c531baa36b52128c392
benwhite0820 <97225493+benwhite0820@users.noreply.github.com>
2023-10-24 17:33:00 +0800

    update button className and type variable name

--- components/button/button.component.tsx
+++ components/button/button.component.tsx
@@ -1,20 +1,35 @@
 'use client';
 
 import './button.style.scss';
-import React from 'react';
+import React, { MouseEventHandler } from 'react';
 
 type Props = {
   children: React.ReactNode;
-  type: 'default' | 'success' | 'cancel';
+  buttonColor: 'default' | 'success' | 'cancel';
+  onClick?: MouseEventHandler<HTMLButtonElement>;
 };
-const Button = ({ children, type }: Props) => {
-  const buttonType = {
+
+const Button = ({ children, buttonColor, onClick }: Props) => {
+  const buttonColorType = {
     default: 'button--default',
     success: 'button--success',
     cancel: 'button--cancel',
   };
 
-  return <button className={`button ${buttonType[type]}`}>{children}</button>;
+  return (
+    <button
+      className={`button ${buttonColorType[buttonColor]}`}
+      disabled={buttonColor === 'default'}
+      onClick={(event) => {
+        if (onClick) {
+          onClick(event);
+        }
+      }}
+      type="submit"
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
